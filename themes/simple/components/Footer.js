891756c3df5c8dc32f4fdeef6729eebad0350ec8
Alex <liushihao1@gmail.com>
2024-05-27 12:08:40 +0800

    修改footer

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -16,7 +16,7 @@ export default function Footer (props) {
         <DarkModeButton className='text-center pt-4'/>
 
         <div className="text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
+            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. <a href="https://beian.miit.gov.cn/">京ICP备2022026944号-1</a> All rights reserved.</div>
             <div className="md:p-0 text-center md:text-right text-xs">
                 {/* 右侧链接 */}
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
